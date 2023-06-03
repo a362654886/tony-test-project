975e3344e164c8f9c6f66e3217913ff37de8b3a9
Lei Lu <41553112+a362654886@users.noreply.github.com>
2023-06-04 04:21:42 +1200

    update

--- playwrightTest/example.spec.ts
+++ playwrightTest/example.spec.ts
@@ -14,44 +14,5 @@ test("homepage has title and links to intro page", async ({ page }) => {
   expect(initialSecondTimerText).toEqual("Second Timer:0.00");
   expect(initialMillionSecondTimerText).toEqual("Million Second Timer: 0.00");
 
-  const button = await page.getByTestId("MainButton");
-
-  const buttonBoundingBox = await button.boundingBox();
-  if (buttonBoundingBox) {
-    const buttonX = buttonBoundingBox.x + buttonBoundingBox.width / 2;
-    const buttonY = buttonBoundingBox.y + buttonBoundingBox.height / 2;
-
-    await page.mouse.move(buttonX, buttonY);
-
-    await page.mouse.down();
-    await page.mouse.up();
-
-    // wait for 5 seconds
-    await page.waitForFunction(
-      () => {
-        return true;
-      },
-      {},
-      { timeout: 5000 }
-    );
-
-    await page.mouse.down();
-
-    await page.waitForFunction(
-      () => {
-        return true;
-      },
-      {},
-      { timeout: 2000 }
-    );
-    await page.mouse.up();
-  }
-
-  const newSecondTimerText = await page
-    .getByTestId("SecondTimerText")
-    .innerText();
-
-  expect(newSecondTimerText).toEqual("Second Timer:0.00");
-
   return;
 });
