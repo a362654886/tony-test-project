0d5f080044ab69bdb9be586afcbf1636df82d220
Lei Lu <41553112+a362654886@users.noreply.github.com>
2023-06-04 04:16:54 +1200

    add playwright test

--- playwrightTest/example.spec.ts
+++ playwrightTest/example.spec.ts
@@ -4,13 +4,54 @@ test("homepage has title and links to intro page", async ({ page }) => {
   //1. go to test page
   await page.goto("https://master.d2wtgak6p58zzd.amplifyapp.com/");
 
-  const secondTimerText = await page.getByTestId("SecondTimerText").innerText();
-  const millionSecondTimerText = await page
+  const initialSecondTimerText = await page
+    .getByTestId("SecondTimerText")
+    .innerText();
+  const initialMillionSecondTimerText = await page
     .getByTestId("MillionSecondTimerText")
     .innerText();
 
-  expect(secondTimerText).toEqual("Second Timer: 0.00");
-  expect(millionSecondTimerText).toEqual("Million Second Timer: 0.00");
+  expect(initialSecondTimerText).toEqual("Second Timer:0.00");
+  expect(initialMillionSecondTimerText).toEqual("Million Second Timer: 0.00");
+
+  const button = await page.getByTestId("MainButton");
+
+  const buttonBoundingBox = await button.boundingBox();
+  if (buttonBoundingBox) {
+    const buttonX = buttonBoundingBox.x + buttonBoundingBox.width / 2;
+    const buttonY = buttonBoundingBox.y + buttonBoundingBox.height / 2;
+
+    await page.mouse.move(buttonX, buttonY);
+
+    await page.mouse.down();
+    await page.mouse.up();
+
+    // wait for 5 seconds
+    await page.waitForFunction(
+      () => {
+        return true;
+      },
+      {},
+      { timeout: 5000 }
+    );
+
+    await page.mouse.down();
+
+    await page.waitForFunction(
+      () => {
+        return true;
+      },
+      {},
+      { timeout: 2000 }
+    );
+    await page.mouse.up();
+  }
+
+  const newSecondTimerText = await page
+    .getByTestId("SecondTimerText")
+    .innerText();
+
+  expect(newSecondTimerText).toEqual("Second Timer:0.00");
 
   return;
 });
